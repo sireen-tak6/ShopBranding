d9acc7f0c4923c1ee7c632b48ff7b8e4022377c6
sireen-tak6 <sita.6.m@gmail.com>
2025-02-18 20:10:20 +0300

    edits

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'shopbranding-production-55a5.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
